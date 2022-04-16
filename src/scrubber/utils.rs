ad89bc78e9f2bfc28a054acf4c934eeb607199b4
Jae Lo Presti (DN0) <me@jae.fi>
2022-04-16 14:06:30 +0300

    Scrubber: add tests

--- src/scrubber/utils.rs
+++ src/scrubber/utils.rs
@@ -10,3 +10,21 @@ pub fn change_file_name(path: impl AsRef<Path>, name: &str) -> PathBuf {
     }
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::scrubber::utils::change_file_name;
+    use std::path::Path;
+    use std::ffi::OsStr;
+
+    #[test]
+    fn change_file_name_works() {
+        let image_path = Path::new("test.jpeg");
+        let filename_stem = image_path.file_stem().unwrap_or(OsStr::new(""));
+        let mut new_filename = filename_stem.to_os_string();
+        new_filename.push("-scrubbed");
+
+        assert_eq!(Path::new("test-scrubbed.jpeg"), change_file_name(image_path, new_filename.to_str().unwrap()));
+    }
+}
+
