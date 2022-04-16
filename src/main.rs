c7de9755c288aa9f50377b9231478ea54f18232c
Jae Lo Presti (DN0) <me@jae.fi>
2022-04-16 04:05:24 +0300

    Main: add working EXIF, XMP and IPTC scrubber

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use std::ffi::OsStr;
+use std::path::PathBuf;
 use clap::Parser;
-use exif::{Reader};
+
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    /// The operation you wish the cli to do (remove, randomize)
-    operation: String,
     /// The image you want to apply the changes to
     #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
@@ -16,16 +17,62 @@ fn main() {
 
     let result = std::fs::File::open(&args.path);
     match result {
-        Ok(content) => { 
+        Ok(_content) => {
             println!("> Found a file, processing!\n");
-            let mut bufreader = std::io::BufReader::new(&content);
-            let exif_reader = Reader::new();
-            let exif = exif_reader.read_from_container(&mut bufreader).unwrap();
-            println!("> Found the following EXIF data:");
-            for f in exif.fields() {
-                println!("{} {} {}", f.tag, f.ifd_num, f.display_value().with_unit(&exif));
+
+            println!("\n> Attempting to clean...\n");
+
+            let meta = rexiv2::Metadata::new_from_path(&args.path).unwrap();
+
+            // EXIF
+            if meta.supports_exif() {
+                println!("> EXIF data found!\n");
+                meta.clear_exif();
+                println!("> Cleared all EXIF data!\n");
+            } else {
+                println!("> No EXIF data found (or not supported)\n.");
             }
+
+            // XMP
+            if meta.has_xmp() {
+                println!("> XMP data found!\n");
+                meta.clear_xmp();
+                println!("> Cleared all XMP data!\n");
+            } else {
+                println!("> No XMP data found (or not supported)\n");
+            }
+
+            // IPTC
+            if meta.has_iptc() {
+                println!("> IPTC data found!\n");
+                meta.clear_iptc();
+                println!("> Cleared all IPTC data!\n");
+            } else {
+                println!("> No IPTC data found (or not supported)\n");
+            }
+
+            // Generate new path for image
+            let filename_stem = &args.path.file_stem().unwrap_or(OsStr::new(""));
+            let mut new_filename = filename_stem.to_os_string();
+            new_filename.push("-scrubbed");
+
+            let new_path = change_file_name(&args.path, new_filename.to_str().unwrap());
+
+            println!("> Saving modified image to {:?}", new_path);
+
+            _ = std::fs::copy(&args.path.as_os_str(), new_path.as_os_str());
+            _ = meta.save_to_file(new_path);
         }
-        Err(error) => { println!("Error: {}", error); }
+        Err(error) => { println!("An error occurred: {}", error); }
     }
+}
+
+fn change_file_name(path: impl AsRef<Path>, name: &str) -> PathBuf {
+    let path = path.as_ref();
+    let mut result = path.to_owned();
+    result.set_file_name(name);
+    if let Some(ext) = path.extension() {
+        result.set_extension(ext);
+    }
+    result
 }
