e8f6d208ac2ec6a81933323dc8fb34d1d3a50791
Jae Lo Presti (DN0) <me@jae.fi>
2022-04-16 13:49:46 +0300

    Scrubber: add option for verbose output

--- src/scrubber.rs
+++ src/scrubber.rs
@@ -3,38 +3,58 @@ use std::ffi::OsStr;
 
 mod utils;
 
-pub fn scrub_image_file(image_path: &std::path::Path, keep_filename: bool) {
-    println!("> Found a path {}, processing!\n", image_path.display());
+pub fn scrub_image_file(image_path: &std::path::Path, keep_filename: bool, verbose: bool) {
+    if verbose {
+        println!("> Found a path {}, processing!\n", image_path.display());
 
-    println!("\n> Attempting to clean...\n");
+        println!("\n> Attempting to clean...\n");
+    }
 
     if let Ok(meta) = Metadata::new_from_path(image_path) {
 
         // EXIF
         if meta.supports_exif() {
-            println!("> EXIF data found!\n");
+            if verbose {
+                println!("> EXIF data found!\n");
+            }
             meta.clear_exif();
-            println!("> Cleared all EXIF data!\n");
+            if verbose {
+                println!("> Cleared all EXIF data!\n");
+            }
         } else {
-            println!("> No EXIF data found (or not supported)\n.");
+            if verbose {
+                println!("> No EXIF data found (or not supported)\n.");
+            }
         }
 
         // XMP
         if meta.has_xmp() {
-            println!("> XMP data found!\n");
+            if verbose {
+                println!("> XMP data found!\n");
+            }
             meta.clear_xmp();
-            println!("> Cleared all XMP data!\n");
+            if verbose {
+                println!("> Cleared all XMP data!\n");
+            }
         } else {
-            println!("> No XMP data found (or not supported)\n");
+            if verbose {
+                println!("> No XMP data found (or not supported)\n");
+            }
         }
 
         // IPTC
         if meta.has_iptc() {
-            println!("> IPTC data found!\n");
+            if verbose {
+                println!("> IPTC data found!\n");
+            }
             meta.clear_iptc();
-            println!("> Cleared all IPTC data!\n");
+            if verbose {
+                println!("> Cleared all IPTC data!\n");
+            }
         } else {
-            println!("> No IPTC data found (or not supported)\n");
+            if verbose {
+                println!("> No IPTC data found (or not supported)\n");
+            }
         }
 
         // Generate new path for image
@@ -44,17 +64,23 @@ pub fn scrub_image_file(image_path: &std::path::Path, keep_filename: bool) {
             new_filename.push("-scrubbed");
 
             let new_path = utils::change_file_name(image_path, new_filename.to_str().unwrap());
-            println!("> Saving modified image to {:?}", new_path);
+            if verbose {
+                println!("> Saving modified image to {:?}", new_path);
+            }
 
             _ = std::fs::copy(image_path.as_os_str(), new_path.as_os_str());
             _ = meta.save_to_file(new_path);
         } else {
-            println!("> Saving modified image to {:?}", image_path);
+            if verbose {
+                println!("> Saving modified image to {:?}", image_path);
+            }
             _ = std::fs::copy(image_path.as_os_str(), image_path.as_os_str());
             _ = meta.save_to_file(image_path);
         }
     } else {
-        println!("> Error: could not scrub image (maybe already scrubbed?)");
+        if verbose {
+            println!("> Error: could not scrub image (maybe already scrubbed?)");
+        }
     }
     
 }
